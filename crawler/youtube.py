dd25d2f495c2496262bf587bd844efdbf19dd6f0
caadiq <caadiq@gmail.com>
2024-05-25 17:15:59 +0900

    유튜브 영상 크롤링

---
+++ crawler/youtube.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+from pytube import YouTube
+
+
+class Videos(BaseModel):
+    videoId: str
+
+
+async def get_details(videos):
+    details = []
+
+    for video in videos:
+        detail = crawling(video.videoId)
+        details.append(detail)
+
+    return details
+
+
+def crawling(video_id):
+    url = f"https://www.youtube.com/watch?v={video_id}"
+    yt = YouTube(url)
+
+    length = yt.length
+    views = yt.views
+
+    return {"length": length, "views": views}
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from fastapi import FastAPI
+from typing import List
+
+from fastapi import FastAPI, HTTPException
+
+from crawler.youtube import Videos, get_details
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.post("/youtube")
+async def youtube_details(videos: List[Videos]):
+    try:
+        return await get_details(videos)
+    except Exception as e:
+        print(f"Error occurred while getting details: {e}")
+        raise HTTPException(status_code=400, detail=str(e))
