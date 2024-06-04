a72801dd32b8cff8acfa2ca8320ab5ed7edd1cb0
caadiq <caadiq@gmail.com>
2024-06-04 13:06:17 +0900

    위버스샵 앨범 추가

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from typing import List
 
 from fastapi import FastAPI, HTTPException
 
+from scrap.weverseshop import get_items
 from scrap.youtube import Videos, get_details
 
 app = FastAPI()
@@ -11,6 +12,15 @@ app = FastAPI()
 async def youtube_details(videos: List[Videos]):
     try:
         return await get_details(videos)
+    except Exception as e:
+        print(f"Error occurred while getting details: {e}")
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+@app.get("/weverseshop")
+async def weverse_shop():
+    try:
+        return await get_items()
     except Exception as e:
         print(f"Error occurred while getting details: {e}")
         raise HTTPException(status_code=400, detail=str(e))
---
+++ scrap/weverseshop.py
@@ -0,0 +1,50 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+async def parse_page(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    items = soup.find_all('li', {'class': 'sc-ee91b503-2 kDyufS'})
+
+    result = []
+    for item in items:
+        title = item.find('a')['title']
+        img_tags = item.find_all('img')
+        if len(img_tags) >= 2:
+            img_src = img_tags[1]['src']
+        else:
+            img_src = img_tags[0]['src']
+        item_url = "https://shop.weverse.io" + item.find('a')['href']
+
+        price = item.find('figcaption', {'class': 'sc-fb17985a-5 dIWEfZ'}).find('strong',
+                                                                                {'class': 'sc-d4956143-2 JDTLz'}).text
+
+        is_sold_out = item.find('figcaption', {'class': 'sc-fb17985a-5 dIWEfZ'}).find('strong', {
+            'class': 'sc-fb17985a-1 ipzbQw'}) is not None
+
+        result.append({'title': title, 'img_src': img_src, 'url': item_url, 'price': price, 'is_sold_out': is_sold_out})
+
+    return result
+
+
+async def get_items():
+    page = 1
+    items = []
+
+    while True:
+        url = f"https://shop.weverse.io/ko/shop/GL_KRW/artists/35/categories/615?page={page}"
+        page_items = await parse_page(url)
+
+        # Check if the page has no items
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        no_items = soup.find('div', {'class': 'sc-eef0fdb-5 iSLDTU'})
+
+        if no_items and "등록된 상품이 없습니다" in no_items.text:
+            break
+
+        items.extend(page_items)
+        page += 1
+
+    return items
