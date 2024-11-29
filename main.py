04fe6313c051f8121f784c9b48bea75197c7a485
반명우 <caadiq@gmail.com>
2024-11-30 00:08:46 +0900

    프로미스나인 계약 종료 예정에 따른 일부 기능 삭제

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ from typing import List
 
 from fastapi import FastAPI, HTTPException
 
-from scrap.dcinside import get_posts
-from scrap.weverse_live import get_lives
-from scrap.weverse_notice import get_notices
-from scrap.weverse_shop import get_albums
 from scrap.youtube import Videos, get_details
 
 app = FastAPI()
@@ -15,42 +11,6 @@ app = FastAPI()
 async def youtube_details(videos: List[Videos]):
     try:
         return await get_details(videos)
-    except Exception as e:
-        print(f"Error occurred while getting details: {e}")
-        raise HTTPException(status_code=400, detail=str(e))
-
-
-@app.get("/weverse/shop")
-async def weverse_shop():
-    try:
-        return await get_albums()
-    except Exception as e:
-        print(f"Error occurred while getting details: {e}")
-        raise HTTPException(status_code=400, detail=str(e))
-
-
-@app.get("/weverse/notice")
-async def weverse_notice():
-    try:
-        return await get_notices()
-    except Exception as e:
-        print(f"Error occurred while getting details: {e}")
-        raise HTTPException(status_code=400, detail=str(e))
-
-
-@app.get("/weverse/live")
-async def weverse_live():
-    try:
-        return await get_lives()
-    except Exception as e:
-        print(f"Error occurred while getting details: {e}")
-        raise HTTPException(status_code=400, detail=str(e))
-
-
-@app.get("/dcinside")
-async def dcinside():
-    try:
-        return await get_posts()
     except Exception as e:
         print(f"Error occurred while getting details: {e}")
         raise HTTPException(status_code=400, detail=str(e))
